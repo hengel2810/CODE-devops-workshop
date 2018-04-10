b1e597c961e86bd842f06c68f2d509f7d6bb4ce2
Henrik Engelbrink <hengel2810@gmail.com>
2018-04-10 12:03:23 +0200

    Fixed unit test

--- app/tests/unit/test_logic.py
+++ app/tests/unit/test_logic.py
@@ -6,6 +6,6 @@ class CalculatorTests(TestCase):
     def test_mul(self):
         c = Calculator()
         res = c.mul(5, 5)
-        self.assertEqual(res, 26)
+        self.assertEqual(res, 25)
     def test_div(self):
         pass
