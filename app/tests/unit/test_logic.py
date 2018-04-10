01a7c2fff379b3b5e8d84b7f3c84aed54ac8e2bc
Henrik Engelbrink <hengel2810@gmail.com>
2018-04-10 11:53:02 +0200

    Added unit test

--- app/tests/unit/test_logic.py
+++ app/tests/unit/test_logic.py
@@ -4,7 +4,8 @@ from calculator.logic import Calculator
 
 class CalculatorTests(TestCase):
     def test_mul(self):
-        pass
-
+        c = Calculator()
+        res = c.mul(5, 5)
+        self.assertEqual(res, 25)
     def test_div(self):
         pass
