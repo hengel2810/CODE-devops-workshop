5e3f8d925e707276c84bef1b20e2deda54b0fced
Henrik Engelbrink <hengel2810@gmail.com>
2018-04-10 12:10:47 +0200

    Added better mul_tests

--- app/tests/unit/test_logic.py
+++ app/tests/unit/test_logic.py
@@ -5,7 +5,19 @@ from calculator.logic import Calculator
 class CalculatorTests(TestCase):
     def test_mul(self):
         c = Calculator()
+        res = c.mul(3, 3)
+        self.assertEqual(res, 9)
+    def test_mul_with_two_positive_numbers(self):
+        c = Calculator()
         res = c.mul(5, 5)
         self.assertEqual(res, 25)
+    def test_mul_with_two_negative_numbers(self):
+        c = Calculator()
+        res = c.mul(-5, -5)
+        self.assertEqual(res, 25)
+    def test_mul_with_one_negative_one_positive_number(self):
+        c = Calculator()
+        res = c.mul(-5, 5)
+        self.assertEqual(res, -25)
     def test_div(self):
         pass
